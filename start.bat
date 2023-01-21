2eaf5caca06f0b80cdeb064939651dc26851e41e
calip728 <115080210+calip728@users.noreply.github.com>
2023-01-21 20:23:27 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ net config server /srvcomment:"Windows Server 2019 By Network Slutter" > out.txt
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user administrator kaliphcuy123 /add >nul
-net localgroup administrators administrator /add >nul
+net localgroup administrators caliph /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get the NGROK tunnel, make sure NGROK_AUTH_TOKEN correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
-echo Username: administrator
+echo Username: caliph
 echo Password: kaliphcuy123
 echo Please Login To Your RDP!!
 ping -n 10 127.0.0.1 >nul
